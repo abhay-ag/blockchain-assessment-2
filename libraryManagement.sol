dc5b155fc7c492efaacb1e92a0bdbadef1f6acc9
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-10-01 20:55:31 +0530

    made the basic structure for book

---
+++ libraryManagement.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.0;
+
+contract libraryManagement{
+    struct book{
+        string name;
+        string author;
+        uint256 price;
+        uint256 quantity;
+    }
+    mapping(uint256 => book) public books;
+    uint256 public bookCount;
+    event bookAdded(uint256 id);
+    event bookRemoved(uint256 id);
+    event bookUpdated(uint256 id);
+    function addBook(string memory _name, string memory _author, uint256 _price, uint256 _quantity) public{
+        bookCount++;
+        books[bookCount] = book(_name, _author, _price, _quantity);
+        emit bookAdded(bookCount);
+    }
+    function removeBook(uint256 _id) public{
+        delete books[_id];
+        emit bookRemoved(_id);
+    }
+    function updateBook(uint256 _id, string memory _name, string memory _author, uint256 _price, uint256 _quantity) public{
+        books[_id] = book(_name, _author, _price, _quantity);
+        emit bookUpdated(_id);
+    }
+
+    
+}
